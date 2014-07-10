d28b37f131dabe06a270c006d67d9bbd0980f71c
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-07-10 15:25:56 -0400

    got recording working - yeah

--- Record/Record/EAFViewController.m
+++ Record/Record/EAFViewController.m
@@ -21,49 +21,100 @@
     _playButton.enabled = NO;
     _stopButton.enabled = NO;
     
+    if (FALSE) {
+        
+        NSLog(@"viewDidLoad");
+        
+        
+        NSArray *dirPaths;
+        NSString *docsDir;
+        
+        dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES);
+        docsDir = dirPaths[0];
+        
+        NSString *soundFilePath = [docsDir stringByAppendingPathComponent:@"sound.caf"];
+        NSURL *soundFileURL = [NSURL fileURLWithPath:soundFilePath];
+        
+        NSDictionary *recordSettings = [NSDictionary
+                                        dictionaryWithObjectsAndKeys:
+                                        [NSNumber numberWithInt:AVAudioQualityMin],
+                                        AVEncoderAudioQualityKey,
+                                        [NSNumber numberWithInt:16],
+                                        AVEncoderBitRateKey,
+                                        [NSNumber numberWithInt: 2],
+                                        AVNumberOfChannelsKey,
+                                        [NSNumber numberWithFloat:44100.0],
+                                        AVSampleRateKey,
+                                        nil];
+        
+        NSError *error = nil;
+        
+        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+        
+        _audioRecorder = [[AVAudioRecorder alloc]
+                          initWithURL:soundFileURL
+                          settings:recordSettings
+                          error:&error];
+        _audioRecorder.delegate = self;
+        _audioRecorder.meteringEnabled = YES;
+        
+        // Disable Stop/Play button when application launches
+        //    NSLog(@"session state%@",audioSession.isActive);
+        
+        if (error)
+        {
+            NSLog(@"error: %@", [error localizedDescription]);
+        } else {
+            [audioSession requestRecordPermission:^(BOOL granted) {
+                NSLog(@"record permission is %hhd", granted);
+            } ];
+            [_audioRecorder prepareToRecord];
+        }
+    }
     
-    NSLog(@"viewDidLoad");
+    NSLog(@"viewDidLoad2 ");
 
-    
-    NSArray *dirPaths;
-    NSString *docsDir;
-    
-    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES);
-    docsDir = dirPaths[0];
-    
-    NSString *soundFilePath = [docsDir stringByAppendingPathComponent:@"sound.caf"];
-    NSURL *soundFileURL = [NSURL fileURLWithPath:soundFilePath];
-    
-    NSDictionary *recordSettings = [NSDictionary
-                                    dictionaryWithObjectsAndKeys:
-                                    [NSNumber numberWithInt:AVAudioQualityMin],
-                                    AVEncoderAudioQualityKey,
-                                    [NSNumber numberWithInt:16],
-                                    AVEncoderBitRateKey,
-                                    [NSNumber numberWithInt: 2],
-                                    AVNumberOfChannelsKey,
-                                    [NSNumber numberWithFloat:44100.0],
-                                    AVSampleRateKey,
-                                    nil];
-    
+    // Set the audio file
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"MyAudioMemo.m4a",
+                               nil];
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
     NSError *error = nil;
     
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    // Setup audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
     
-    _audioRecorder = [[AVAudioRecorder alloc]
-                      initWithURL:soundFileURL
-                      settings:recordSettings
-                      error:&error];
     if (error)
     {
         NSLog(@"error: %@", [error localizedDescription]);
     } else {
-        [audioSession requestRecordPermission:^(BOOL granted) {
+        [session requestRecordPermission:^(BOOL granted) {
             NSLog(@"record permission is %hhd", granted);
         } ];
-        //[_audioRecorder prepareToRecord];
+        // [_audioRecorder prepareToRecord];
     }
+    
+    // Define the recorder setting
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+    
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    _audioRecorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:&error];
+    
+    if (error)
+    {
+        NSLog(@"error: %@", [error localizedDescription]);
+    }
+    
+    _audioRecorder.delegate = self;
+    _audioRecorder.meteringEnabled = YES;
+    [_audioRecorder prepareToRecord];
 }
 
 - (void)didReceiveMemoryWarning
@@ -104,6 +155,13 @@
 
         _playButton.enabled = NO;
         _stopButton.enabled = YES;
+        
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        
+        NSError *error = nil;
+
+        [session setActive:YES error:&error];
+
         [_audioRecorder record];
         
         if (_audioRecorder.recording)
@@ -112,7 +170,11 @@
         }
         else {
             NSLog(@"recordAudio -DUDE NOT recording");
-
+            
+            NSLog(@"Domain: %@", error.domain);
+            NSLog(@"Error Code: %d", error.code);
+            NSLog(@"Description: %@", [error localizedDescription]);
+            NSLog(@"Reason: %@", [error localizedFailureReason]);
         }
     }
 }
