19e5af7ea1982e2b3930ddcc2819395dc58ad226
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-06-29 18:05:12 -0400

    changed highlighting to change the background color to highlight while audio cursor is over word or phone

--- Record/Record/EAFRecoFlashcardController.m
+++ Record/Record/EAFRecoFlashcardController.m
@@ -59,10 +59,7 @@
 @property NSArray *wordTranscript;
 @property NSArray *phoneTranscript;
 @property NSMutableArray *wordLabels;
-//@property NSMutableArray *phoneStarts;
-//@property NSMutableArray *phoneEnds;
 @property NSMutableArray *phoneLabels;
-//@property NSMutableDictionary *exToResponse;
 - (void)postAudio;
 
 @end
@@ -208,12 +205,6 @@
     
     // Setup audio session
     AVAudioSession *session = [AVAudioSession sharedInstance];
-    
-//    [session setCategory:AVAudioSessionCategoryPlayAndRecord error: nil];
-//    UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
-//    AudioSessionSetProperty (kAudioSessionProperty_OverrideAudioRoute,
-//                             sizeof (audioRouteOverride),&audioRouteOverride);
-    
     
     [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&setCategoryError];
     
@@ -1228,7 +1219,43 @@ BOOL preventPlayAudio = false;
    // NSLog(@" stopPlayingAudio");
     [self removePlayingAudioHighlight];
     [_myAudioPlayer stopAudio];
+    [_altPlayer pause];
     [_synthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
+    
+    int i = 0;
+    
+    for (NSDictionary *event in _wordTranscript) {
+        NSString *word = [event objectForKey:@"event"];
+        if ([word isEqualToString:@"sil"] || [word isEqualToString:@"<s>"] || [word isEqualToString:@"</s>"]) continue;
+        NSNumber *wstart = [event objectForKey:@"start"];
+        NSNumber *wend   = [event objectForKey:@"end"];
+        NSNumber *wscore   = [event objectForKey:@"score"];
+        // NSLog(@"w on %d of %d",i,weakSelf.wordLabels.count);
+        // NSLog(@"p on %d of %d",i,weakSelf.phoneLabels.count);
+        
+        UILabel *label      = [_wordLabels  objectAtIndex:i];
+        UILabel *phoneLabel = [_phoneLabels objectAtIndex:i++];
+
+        // put back the coloring on the word labels
+        NSMutableAttributedString *highlight = [[NSMutableAttributedString alloc] initWithString:label.text];
+        
+        UIColor *hColor = [self getColor2:[wscore floatValue]];
+        NSRange range = NSMakeRange(0, [highlight length]);
+        
+        [highlight addAttribute:NSBackgroundColorAttributeName
+                          value:hColor
+                          range:range];
+        
+        label.attributedText = highlight;
+        
+        //      NSLog(@"event %@ at %f - %f not in %f",word,[wstart floatValue],[wend floatValue],now);
+        
+        // put back the coloring on the phone labels
+        NSString *phoneToShow = [self getPhonesWithinWord:wend wstart:wstart phoneAndScore:self.phoneTranscript];
+        NSMutableAttributedString *coloredPhones = [self getColoredPhones:phoneToShow wend:wend wstart:wstart phoneAndScore:self.phoneTranscript];
+        phoneLabel.attributedText = coloredPhones;
+    }
+    
 }
 
 - (void)removePlayingAudioHighlight {
@@ -1426,18 +1453,35 @@ bool debugRecord = false;
         
         _altPlayer = [[AVPlayer alloc] initWithURL:_audioRecorder.url];
         
-        CMTime tm = CMTimeMakeWithSeconds(0.02, 100);
+        CMTime tm = CMTimeMakeWithSeconds(0.01, 100);
         _lastUpdate = 0.0;
         
         __weak typeof(self) weakSelf = self;
+
+        NSMutableDictionary *prevWordAttr  = [NSMutableDictionary new];
+        
+        int i = 0;
+        for (NSDictionary *event in weakSelf.wordTranscript) {
+            NSString *word = [event objectForKey:@"event"];
+            if ([word isEqualToString:@"sil"] || [word isEqualToString:@"<s>"] || [word isEqualToString:@"</s>"]) continue;
+            // NSLog(@"w on %d of %d",i,weakSelf.wordLabels.count);
+            //   NSLog(@"p on %d of %d",i,weakSelf.phoneLabels.count);
+            
+            UILabel *label      = [weakSelf.wordLabels  objectAtIndex:i];
+            UILabel *phoneLabel = [weakSelf.phoneLabels objectAtIndex:i];
+            
+            [prevWordAttr  setObject:label.attributedText forKey:[NSNumber numberWithInt:i]];
+         //   [prevPhoneAttr setObject:phoneLabel.attributedText forKey:[NSNumber numberWithInt:i]];
+            i++;
+        }
+        
+        UIColor *hColor = [EAFRecoFlashcardController colorFromHexString:@"#007AFF"];
         
         [_altPlayer addPeriodicTimeObserverForInterval:tm queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
-            //  float duration = CMTimeGetSeconds(_altPlayer.currentItem.duration);
             float now = CMTimeGetSeconds(time);
            // NSLog(@"Got time updated %f",now);
             if (now == weakSelf.lastUpdate && now > 0) {
            //     NSLog(@"Got to end!");
-                //  [weakSelf setTextColor:[UIColor blueColor]];
                 weakSelf.recordButton.enabled = YES;
                 [weakSelf setTextColor:[UIColor blackColor]];
             }
@@ -1446,68 +1490,50 @@ bool debugRecord = false;
                 for (NSDictionary *event in weakSelf.wordTranscript) {
                     NSString *word = [event objectForKey:@"event"];
                     if ([word isEqualToString:@"sil"] || [word isEqualToString:@"<s>"] || [word isEqualToString:@"</s>"]) continue;
-                    // NSNumber *score = [event objectForKey:@"score"];//saidWord ? [event objectForKey:@"score"] : 0;
                     NSNumber *wstart = [event objectForKey:@"start"];
-                    NSNumber *wend  = [event objectForKey:@"end"];
+                    NSNumber *wend   = [event objectForKey:@"end"];
                    // NSLog(@"w on %d of %d",i,weakSelf.wordLabels.count);
                  //   NSLog(@"p on %d of %d",i,weakSelf.phoneLabels.count);
                     
                     UILabel *label      = [weakSelf.wordLabels  objectAtIndex:i];
+                    NSNumber *index = [NSNumber numberWithInt:i];
                     UILabel *phoneLabel = [weakSelf.phoneLabels objectAtIndex:i++];
                     
                     if (now >= [wstart floatValue] && now < [wend floatValue]) {
-                        [label setTextColor:[UIColor blueColor]];
+                        //[label setTextColor:[UIColor blueColor]];
+                     //   NSLog(@"hilight text %@",label.text);
+                        NSMutableAttributedString *highlight = [[NSMutableAttributedString alloc] initWithString:label.text];
+                        
+                        NSRange range = NSMakeRange(0, [highlight length]);
+                        
+                        [highlight addAttribute:NSBackgroundColorAttributeName
+                                          value:hColor
+                                          range:range];
+                        
+                        label.attributedText = highlight;
+                        
                         NSAttributedString *attr =
                         [weakSelf markForegroundPhones:wend wstart:wstart phoneAndScore:weakSelf.phoneTranscript current:phoneLabel.attributedText now:now];
+                        
                         phoneLabel.attributedText = attr;
-                        break;
+                       // break;
                     }
                     else {
-                        NSMutableAttributedString *coloredPhones = [[NSMutableAttributedString alloc] initWithAttributedString:phoneLabel.attributedText];
-                        //    NSLog(@"current %@ at %d",current,[current length]);
+                      //  NSLog(@"don't highlight %@",label.text);
+
+                        label.attributedText = [prevWordAttr objectForKey:index];
                         
-                        //[coloredPhones beginEditing];
-                       // __block BOOL found = NO;
-                        [coloredPhones enumerateAttribute:NSForegroundColorAttributeName inRange:NSMakeRange(0, coloredPhones.length) options:0 usingBlock:^(id value, NSRange range, BOOL *stop) {
-                            if (value) {
-                                //   UIFont *oldFont = (UIFont *)value;
-                                //   UIFont *newFont = [oldFont fontWithSize:oldFont.pointSize * 2];
-                                [coloredPhones removeAttribute:NSForegroundColorAttributeName range:range];
-                                //   [res addAttribute:NSFontAttributeName value:newFont range:range];
-                              //  found = YES;
-                            }
-                        }];
+                        NSMutableAttributedString *coloredPhones = [weakSelf getColoredPhones:phoneLabel.text wend:wend wstart:wstart phoneAndScore:weakSelf.phoneTranscript];
                         phoneLabel.attributedText = coloredPhones;
 
                   //      NSLog(@"event %@ at %f - %f not in %f",word,[wstart floatValue],[wend floatValue],now);
-                        [label setTextColor:[UIColor blackColor]];
+//                        [label setTextColor:[UIColor blackColor]];
                     }
                 }
-//                
-//                int j = 0;
-//                for (NSDictionary *event in weakSelf.phoneTranscript) {
-//                    NSString *word = [event objectForKey:@"event"];
-//                    if ([word isEqualToString:@"sil"] || [word isEqualToString:@"<s>"] || [word isEqualToString:@"</s>"]) continue;
-//                    // NSNumber *score = [event objectForKey:@"score"];//saidWord ? [event objectForKey:@"score"] : 0;
-//                    NSNumber *wstart = [event objectForKey:@"start"];
-//                    NSNumber *wend  = [event objectForKey:@"end"];
-//                    UILabel *label = [weakSelf.phoneLabels objectAtIndex:j++];
-//                    if (now >= [wstart floatValue] && now < [wend floatValue]) {
-//                        [label setTextColor:[UIColor blueColor]];
-//                        break;
-//                    }
-//                    else {
-//                        //      NSLog(@"event %@ at %f - %f not in %f",word,[wstart floatValue],[wend floatValue],now);
-//                        [label setTextColor:[UIColor blackColor]];
-//                    }
-//                }
             }
             
             weakSelf.lastUpdate = now;
-            //NSLog(@"Got status %f",);
         }];
-        
-        //[self setTextColor:[UIColor blueColor]];
         
         if (error)
         {
@@ -1525,6 +1551,14 @@ bool debugRecord = false;
         
         [self postEvent:@"playUserAudio" widget:@"userScoreDisplay" type:@"UIView"];
    }
+}
+
++ (UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
 
 - (IBAction)stopAudio:(id)sender {
@@ -1820,8 +1854,6 @@ bool debugRecord = false;
         [self setDisplayMessage:@"Server error, please report."];
         return;
     }
-
-  //  [_exToResponse setObject:json forKey:exid];
     
     NSNumber *previousScore;
     if (exid != nil) {
@@ -1882,9 +1914,6 @@ bool debugRecord = false;
     }
     
     wordLabel.attributedText = coloredWord;
-    wordLabel.font = _foreignLang.font; // font sizes should match
-    
-    
     wordLabel.font  = wordFont;
     
 //    if ([self isiPhone]// && [_foreignLang.text length] > 15
@@ -1928,8 +1957,6 @@ BOOL addSpaces = false;
             [coloredPhones addAttribute:NSBackgroundColorAttributeName
                                   value:color
                                   range:range];
-            //   [_phoneStarts addObject:start];
-            //   [_phoneEnds   addObject:end];
         }
         else {
             
@@ -1938,65 +1965,51 @@ BOOL addSpaces = false;
     return coloredPhones;
 }
 
+// So here we set the background color for the phone to be highlight blue when the audio cursor
+// is inside the phone region, otherwise, we use the scored color.
 -  (NSMutableAttributedString *) markForegroundPhones:(NSNumber *)wend wstart:(NSNumber *)wstart
                                         phoneAndScore:(NSArray *)phoneAndScore
                                               current:(NSAttributedString *)current
                                                   now:(float) now
 {
     // now mark the ranges in the string with colors
-    
     NSMutableAttributedString *coloredPhones = [[NSMutableAttributedString alloc] initWithAttributedString:current];
-//    NSLog(@"current %@ at %d",current,[current length]);
+ //   NSLog(@"current %d ",[current length]);
 
-    //[coloredPhones beginEditing];
-    __block BOOL found = NO;
-    [coloredPhones enumerateAttribute:NSForegroundColorAttributeName inRange:NSMakeRange(0, coloredPhones.length) options:0 usingBlock:^(id value, NSRange range, BOOL *stop) {
-        if (value) {
-            //   UIFont *oldFont = (UIFont *)value;
-            //   UIFont *newFont = [oldFont fontWithSize:oldFont.pointSize * 2];
-            [coloredPhones removeAttribute:NSForegroundColorAttributeName range:range];
-            //   [res addAttribute:NSFontAttributeName value:newFont range:range];
-            found = YES;
-        }
-    }];
-    
-    if (!found) {
-       // NSLog(@"didn't find it!");
-        // No font was found - do something else?
-    }
+    UIColor *hColor = [EAFRecoFlashcardController colorFromHexString:@"#007AFF"];
     
     int pstart = 0;
     for (NSDictionary *event in phoneAndScore) {
         NSString *phoneText = [event objectForKey:@"event"];
         if ([phoneText isEqualToString:@"sil"]) continue;
-   //     NSLog(@"phone %@ at %d",phoneText,pstart);
+  //      NSLog(@"phone %@ at %d",phoneText,pstart);
         
-        //        NSNumber *pscore = [event objectForKey:@"score"];
         NSNumber *start  = [event objectForKey:@"start"];
         NSNumber *end    = [event objectForKey:@"end"];
         
         if ([start floatValue] >= [wstart floatValue] && [end floatValue] <= [wend floatValue]) {
             NSRange range = NSMakeRange(pstart, [phoneText length]);
-         //   NSLog(@"pstart %@ at %d",phoneText,pstart);
+   //         NSLog(@"\tpstart %@ at %d %d",phoneText,pstart,i);
             pstart += range.length + (addSpaces ? 1 : 0);
+            NSNumber *pscore = [event objectForKey:@"score"];
 
             if (now >= [start floatValue] && now < [end floatValue]) {
           //      NSLog(@"%f-%f in %f phone %@ range %@",[start floatValue],[end floatValue],now,phoneText,NSStringFromRange(range));
-                //  float score = [pscore floatValue];
-                //  UIColor *color = [self getColor2:score];
-                //        NSLog(@"%@ %f %@ range at %lu length %lu", phoneText, score,color,(unsigned long)range.location,(unsigned long)range.length);
-                [coloredPhones addAttribute:NSForegroundColorAttributeName
-                                      value:[UIColor blueColor]
+                [coloredPhones addAttribute:NSBackgroundColorAttributeName
+                                      value:hColor
                                       range:range];
-                // [_phoneStarts addObject:start];
-                //[_phoneEnds   addObject:end];
             }
-            
+            else {
+                float score = [pscore floatValue];
+                UIColor *color = [self getColor2:score];
+                [coloredPhones addAttribute:NSBackgroundColorAttributeName
+                                      value:color
+                                      range:range];
+            }
         }
     }
     return coloredPhones;
 }
-
 
 - (BOOL)isRTL {
     NSArray *rtl = [NSArray arrayWithObjects: @"Dari",
@@ -2024,7 +2037,7 @@ BOOL addSpaces = false;
         NSString *phone = [event objectForKey:@"event"];
         if ([phone isEqualToString:@"sil"]) continue;
         NSNumber *start = [event objectForKey:@"start"];
-        NSNumber *end = [event objectForKey:@"end"];
+        NSNumber *end   = [event objectForKey:@"end"];
         
         if ([start floatValue] >= [wstart floatValue] && [end floatValue] <= [wend floatValue]) {
             phoneToShow = [phoneToShow stringByAppendingString:phone];
