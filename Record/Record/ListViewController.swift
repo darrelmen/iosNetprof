52cf267c812ab13bdcb48ef46df7fe89a2de3650
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2019-01-09 13:11:41 -0500

    shows available lists

--- Record/Record/ListViewController.swift
+++ Record/Record/ListViewController.swift
@@ -9,18 +9,10 @@
 import Foundation;
 import UIKit;
 
-
-
 // show available lists
 class ListViewController: UITableViewController {
-    
-    //@objc public var language:String = ""
-    //@objc public var url:String = ""
-    //@objc public var isRTL:Bool = false
-    
     var ids:[Int]=[]
     var names:[String]=[]
-   
     
     override init(style : UITableView.Style) {
         // Overriding this method prevents other initializers from being inherited.
@@ -37,78 +29,45 @@ class ListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let siteGetter = EAFGetSites()
-        let serverURL = siteGetter.getServerURL()
+        let serverURL = EAFGetSites().getServerURL()
         let server = serverURL! + "scoreServlet?lists"
-        //print("ListViewController server url \(server)")
         
         let request = NSMutableURLRequest(url: NSURL(string: server)! as URL)
-        //let session = URLSession.shared
         request.httpMethod = "GET"
         request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         
         let queue:OperationQueue = OperationQueue()
         
-        //UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
         
         NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: queue, completionHandler:{ (response: URLResponse?, data: Data?, error: Error?) -> Void in
-            
             do {
                 if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
-                  //  UIApplication.shared.isNetworkActivityIndicatorVisible = false
-                    print("ASynchronous\(jsonResult)")
-                    self.addItems(json: jsonResult);
+                    DispatchQueue.main.async {
+                         UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                        self.addItems(json: jsonResult);
+                    }
                 }
             } catch let error as NSError {
-               // UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                DispatchQueue.main.async {
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                }
                 print(error.localizedDescription)
             }       
             
         })
-        
-//        let task = session.dataTask(with: request as URLRequest, completionHandler: {(data, response, error) in
-//            if (error != nil) {
-//                print("ListViewController error \(error)")
-//            }
-//            else {
-//                print("ListViewController Response: \(response)")
-//            }
-//        })
-//
-//        task.resume()
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     func addItems(json : NSDictionary) {
-//        for (key,value) in json {
-//            print("key  \(key)")
-//            print("value \(value)")
-//
-//            let listPairs = value as NSArray
-//            for (id,name) in value {
-//
-//            }
-//        }
-        
         if let dictionary = json as? [String: Any] {
             if let nestedDictionary = dictionary["lists"] as? [Any] {
                 // access nested dictionary values by key
-                
-                if let array = nestedDictionary as? [Any] {
-//                    if let firstObject = array.first {
-//                        // access individual object in array
-//                        print("firstObject  \(firstObject)")
-//                            }
-//
-                    for object in array {
+                //if let array = nestedDictionary as? [Any] {
+                    for object in nestedDictionary {
                         // access all objects in array
-                        print("object  \(object) \(type(of: object))")
-                        //let type = type(of: object)
                         
-
                         if let obj = object as? [String:Any] {
-                         
                             if let id = obj["id"] as? Int {
                                 ids.append(id)
                             }
@@ -116,28 +75,26 @@ class ListViewController: UITableViewController {
                                 names.append(name)
                             }
                         }
-                      //  print("now \(ids.count)")
-                     //   print("value \(value)")
                     }
-                    
-//                    for case let string as String in array {
-//                        // access only string values in array
-//                    }
-                }
-                
-//                for (key, value) in nestedDictionary {
-//                    // access all key / value pairs in dictionary
-//                    print("key  \(key)")
-//                    print("value \(value)")
-//                }
-                
+                //}
             }
         }
-
-//
-//        tableView.beginUpdates()
-//         tableView.insertRows(at: [IndexPath(row: names.count-1, section: 0)], with: .automatic)
-//        tableView.endUpdates()
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return names.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListPrototypeCell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -154,6 +111,4 @@ class ListViewController: UITableViewController {
         //        if (segue.identifier == 'g)
         
     }
-    
-    
 }
